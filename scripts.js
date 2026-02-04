09fde8add9884b514c012b00bf06354adfc4de75
RakshithNM <bellare545@gmail.com>
2026-02-04 20:00:28 +0530

    Separate year pages, removed code duplication, removed dialog related code

--- scripts.js
+++ scripts.js
@@ -1,30 +1,3 @@
-/* Colorize the website title */
-const rainbowColor = () => {
-  const getColor = (inIndex, inRange) => {
-    return Math.trunc(inIndex / inRange);
-  };
-
-  const colorize = (inElement) => {
-    if(inElement) {
-      const wordArr = inElement.textContent.split("");
-      inElement.textContent = "";
-      const range = Math.round(wordArr.length / 6);
-      for(let index in wordArr) {
-        const span = document.createElement('span');
-        span.textContent = wordArr[Number(index)];
-        const color = getColor(Number(index), range);
-        span.style.setProperty('--hue', 60 * color);
-        inElement.append(span);
-      }
-    }
-  };
-
-  const nameElements = document.querySelectorAll('[data-type="name"]');
-  for(const nameElement of nameElements) {
-    colorize(nameElement);
-  }
-};
-
 /* Count up the remaining funds */
 const countUp = () => {
   const funds = document.querySelectorAll('.funds');
@@ -47,19 +20,6 @@ const countUp = () => {
 };
 
 const handleDialogEvents = () => {
-  const years = ["2022", "2023", "2024", "2025"];
-  for(const year of years) {
-    const openButton = document.getElementById(`dialogOpener${year}`);
-    const dialogElem = document.getElementById(`dialog${year}`);
-    const closeButton = document.getElementById(`dialogClose${year}`);
-    openButton.addEventListener('click', () => {
-      dialogElem.showModal();
-    });
-    closeButton.addEventListener('click', () => {
-      dialogElem.close();
-    });
-  }
-
   const gratitudeOpener = document.getElementById('gratitudeOpener');
   const gratitudeDialog = document.getElementById('gratitudeDialog');
   const gratitudeClose = document.getElementById('gratitudeClose');
@@ -75,6 +35,9 @@ const handleDialogEvents = () => {
 
 const animateRemainingFunds = () => {
   const funds = document.getElementById('remaining-funds');
+  if(!funds) {
+    return;
+  }
   const observerCallback = (entries) => {
     entries.forEach((entry) => {
       if(entry.isIntersecting) {
@@ -88,12 +51,14 @@ const animateRemainingFunds = () => {
 
 const updateDurationInYears = () => {
   const year = document.getElementById("durationInYears");
+  if(!year) {
+    return;
+  }
   const startYear = 2016;
   const currentYear = new Date().getFullYear();
   year.innerText = currentYear - startYear;
 };
 
-rainbowColor();
 handleDialogEvents();
 animateRemainingFunds();
 updateDurationInYears();
